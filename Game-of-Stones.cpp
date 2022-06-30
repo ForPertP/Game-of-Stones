5958e221119fa1f4ecbecb2b5bc7c395bf3da766
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-06-30 12:14:50 +0900

    Update Game-of-Stones.cpp

--- Game-of-Stones.cpp
+++ Game-of-Stones.cpp
@@ -1,1 +1,33 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'gameOfStones' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER n as parameter.
+ */
+
+string gameOfStones(int n) {
+
+}#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'gameOfStones' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER n as parameter.
+ */
+
+string gameOfStones(int n) {
+
+}
