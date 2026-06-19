eff70fe32e046551bd8f01eca2fb955e8aa9721c
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-06-20 00:28:20 +0900

    Update Game-of-Stones.cs

--- Game-of-Stones.cs
+++ Game-of-Stones.cs
@@ -1,1 +1,47 @@
-Game-of-Stones
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'gameOfStones' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER n as parameter.
+     */
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            string result = Result.gameOfStones(n);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
