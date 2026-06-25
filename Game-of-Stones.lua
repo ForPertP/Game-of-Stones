e8daff5558baf3da806e0e0326b4eca5e6b79eb5
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-06-26 02:06:45 +0900

    Update Game-of-Stones.lua

--- Game-of-Stones.lua
+++ Game-of-Stones.lua
@@ -1,4 +1,4 @@
-Game-of-Stones
+-- Game-of-Stones
 
 
 
@@ -8,3 +8,20 @@ Game-of-Stones
 -- The function is expected to return a STRING.
 -- The function accepts INTEGER n as parameter.
 --
+
+
+
+
+local fptr = io.open(os.getenv("OUTPUT_PATH"), "w")
+
+local t = io.stdin:read("*n", "*l")
+
+for titr = 1, t do
+    local n = io.stdin:read("*n", "*l")
+
+    local result = gameOfStones(n)
+
+    fptr:write(result, "\n")
+end
+
+fptr:close()
