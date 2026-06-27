fbdec5176c8c303e60b5426ea94b04954e5934c8
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-06-28 01:17:45 +0900

    Update Game-of-Stones.lua

--- Game-of-Stones.lua
+++ Game-of-Stones.lua
@@ -1,5 +1,3 @@
--- Game-of-Stones
-
 
 
 --
@@ -8,6 +6,29 @@
 -- The function is expected to return a STRING.
 -- The function accepts INTEGER n as parameter.
 --
+
+function gameOfStones(n)
+    local dp = {}
+
+    for i = 0, n do
+        dp[i] = false
+    end
+
+    for i = 2, n do
+        if (i >= 2 and not dp[i - 2]) or
+           (i >= 3 and not dp[i - 3]) or
+           (i >= 5 and not dp[i - 5]) then
+            dp[i] = true
+        end
+    end
+
+    if dp[n] then
+        return "First"
+    else
+        return "Second"
+    end
+end
+
 
 function gameOfStones2(n)
     if n % 7 < 2 then
